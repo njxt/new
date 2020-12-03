82f13002d3eb3e54dc2d2fd7dd9915be16c0c4b9
ZidaruRobert <66086654+ZidaruRobert@users.noreply.github.com>
2020-12-03 09:40:46 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const title = 'Ｂｅａｓｔ',
-    loading = ['','','-',''];
+    loading = ['|','/','-','\\'];
 
 var loadingChar = loading[0],
     currentTitle = title,
@@ -8,16 +8,16 @@ var loadingChar = loading[0],
     c = 0,
     d = 1;
 
-var titleInterval = setInterval(() = {
+var titleInterval = setInterval(() => {
     loadingChar = loading[c % 4];
     if (d == 1) {
         currentTitle = title.substring(a%title.length, title.length);
-        if (a%title.length == title.length-1) d = -1;
-        document.title = `${loadingChar} ${currentTitle.length  5  currentTitle + 'u2000'.repeat(2(5-currentTitle.length))  currentTitle} ${loadingChar}`;
+        if (a%title.length == title.length-1) d *= -1;
+        document.title = `${loadingChar} ${currentTitle.length < 5 ? currentTitle + '\u2000'.repeat(2*(5-currentTitle.length)) : currentTitle} ${loadingChar}`;
     } else {
         currentTitle = title.substring(0, a%title.length);
-        if (a%title.length == title.length-1) d = -1;
-        document.title = `${loadingChar} ${currentTitle.length  5  'u2000'.repeat(2(5-currentTitle.length)) + currentTitle  currentTitle} ${loadingChar}`;
+        if (a%title.length == title.length-1) d *= -1;
+        document.title = `${loadingChar} ${currentTitle.length < 5 ? '\u2000'.repeat(2*(5-currentTitle.length)) + currentTitle : currentTitle} ${loadingChar}`;
     }
     a += b;
     c++;
@@ -28,8 +28,8 @@ var canvas = document.getElementById('starfield'),
     starAmount = 150,
     stars = [];
 
-canvas.width = window.innerWidth0.99;
-canvas.height = window.innerHeight0.99;
+canvas.width = window.innerWidth*0.99;
+canvas.height = window.innerHeight*0.99;
 
 class Star {
     constructor(x,y) {
@@ -37,31 +37,31 @@ class Star {
         this.y = y;
         this.minSpeed = 0.5;
         this.maxSpeed = 1.5;
-        this.dirVelocity = [Math.random()  0.5  1  -1, Math.random()  0.5  1  -1];
-        this.velocity = [Math.max(Math.random()this.maxSpeed,this.minSpeed)this.dirVelocity[0], Math.max(Math.random()this.maxSpeed,this.minSpeed)this.dirVelocity[1]];
+        this.dirVelocity = [Math.random() > 0.5 ? 1 : -1, Math.random() > 0.5 ? 1 : -1];
+        this.velocity = [Math.max(Math.random()*this.maxSpeed,this.minSpeed)*this.dirVelocity[0], Math.max(Math.random()*this.maxSpeed,this.minSpeed)*this.dirVelocity[1]];
     }
 
     setVelocity(x, y, speed) {
-        this.dirVelocity =[x  0  1  -1, y  0  1  -1];
+        this.dirVelocity =[x > 0 ? 1 : -1, y > 0 ? 1 : -1];
         this.velocity = [speed, speed];
     }
 
     move() {
         this.velocity = [
-            Math.abs(Math.min(Math.max(this.velocity[0]0.9, this.minSpeed), this.maxSpeed))this.dirVelocity[0],
-            Math.abs(Math.min(Math.max(this.velocity[1]0.9, this.minSpeed), this.maxSpeed))this.dirVelocity[1]
+            Math.abs(Math.min(Math.max(this.velocity[0]*0.9, this.minSpeed), this.maxSpeed))*this.dirVelocity[0],
+            Math.abs(Math.min(Math.max(this.velocity[1]*0.9, this.minSpeed), this.maxSpeed))*this.dirVelocity[1]
         ];
 
         this.x += this.velocity[0];
         this.y += this.velocity[1];
 
-        if (this.x = 0)
+        if (this.x <= 0)
             this.x = canvas.width;
-        if (this.y = 0)
+        if (this.y <= 0)
             this.y = canvas.height;
-        if (this.x  canvas.width)
+        if (this.x > canvas.width)
             this.x = 0;
-        if (this.y  canvas.height)
+        if (this.y > canvas.height)
             this.y = 0;
     }
 
@@ -70,7 +70,7 @@ class Star {
     show(size, color) {
         context.beginPath();
         context.fillStyle = color;
-        context.arc(this.x, this.y, size, 0, Math.PI2);
+        context.arc(this.x, this.y, size, 0, Math.PI*2);
         context.fill();
     }
 }
@@ -90,9 +90,9 @@ function onMouseUpdate(e) {
     mouseY = e.pageY;
 }
 
-for (let i = 0; i  starAmount; i++) {
-    let x = Math.random()canvas.width;
-    let y = Math.random()canvas.height;
+for (let i = 0; i < starAmount; i++) {
+    let x = Math.random()*canvas.width;
+    let y = Math.random()*canvas.height;
     let star = new Star(x ,y);
     stars.push(star);
 }
@@ -100,9 +100,9 @@ for (let i = 0; i  starAmount; i++) {
 var lastTime = Date.now();
 
 function draw() {
-    canvas.width = window.innerWidth0.99;
-    canvas.height = window.innerHeight0.99;
-    context.fillStyle=black;
+    canvas.width = window.innerWidth*0.99;
+    canvas.height = window.innerHeight*0.99;
+    context.fillStyle="black";
     context.fillRect(0,0,canvas.width,canvas.height);
     for (star of stars) {
         let distX = (mouseX-star.x);
@@ -110,7 +110,7 @@ function draw() {
 
         let dist = 100;
 
-        if (Math.abs(distX)  dist && Math.abs(distY)  dist) {           
+        if (Math.abs(distX) < dist && Math.abs(distY) < dist) {           
             star.setVelocity(distX, distY, Math.abs(deltaX+deltaY));
 
             context.beginPath();
@@ -130,11 +130,11 @@ function draw() {
 }
 
 function update() {
-    if (Date.now()-lastTime  30)  {
+    if (Date.now()-lastTime > 30)  {
         draw();
         lastTime = Date.now();
     }
     window.requestAnimationFrame(update);
 }
 update();
-console.log(LOLSK)
+console.log("LOLSK")
